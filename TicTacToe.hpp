f3ac1da40915210e6aaea130c8a4c9dfec7b5bde
hanzemeng <51301998+hanzemeng@users.noreply.github.com>
2020-11-08 19:04:41 -0500

    elaborated on checkVictory(); explained field's indexing (#21)
    * elaborated on checkVictory(); explained field's indexing
    
    * added *; merged a line

--- TicTacToe.hpp
+++ TicTacToe.hpp
@@ -30,16 +30,20 @@ class TicTacToe
 		// These functions should only be called from within this class.
 		void changeTurn(); //Phillip
 		void checkVictory();
+		// Below three functions will be called within checkVictory(); take teamID as parameter to count score for that team; return socre.
+		int checkVictoryHorizontal(int teamSide); // check all (18) horizontal rows; 9 for left to right, 9 for front to back.
+		int checkVictoryVertical(int teamSide); // check all (9) vertical rows; 9 for bottom to top.
+		int checkVictoryDiagonal(int teamSide); // check all (22) diagonal rows; 6 6 6 4 (I don't know how to describe them with words).
 
 		// Stores the state of the game board
-		// * Index 0 is the bottom and index 2 is top of each stack.
+		// * For each deque<int>, index 0 is the bottom and index 2 is top of each stack.
+		// * For the entire field, [0][0] is bottom left and [2][2] is top right.
 		std::deque<int> field[3][3];
 
 		// Flag determining whose turn it is:
 		int teamSide;
 
-		// Decrementing counter for keeping track of how many actions are left in
-		// the player's turn.
+		// Decrementing counter for keeping track of how many actions are left in the player's turn.
 		int actionsLeft;
 
 		// Stores whether or not there has been a victory and which team has won.
