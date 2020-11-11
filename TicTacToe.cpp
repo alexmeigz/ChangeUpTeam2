dd3033efecaa66d6884887d603b396152a4b9a13
Vedad Bassari <60596059+vedad-bassari@users.noreply.github.com>
2020-11-11 15:33:42 -0800

    Header Update #1
    The header does the following:
    - Select a random side to begin the game.
    - Assign the number of actions left (2 in the first turn).
    
    Please let me know if the header should do anything else, or if the script is problematic. Thank you.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,6 +1,22 @@
 #include "TicTacToe.h"
+#include <time.h>
 
 TicTacToe::TicTacToe()
 {
+    srand(time(NULL));  // initialize random number generator
+    int a = rand();     // generate random number
+    int b = RAND_MAX / 2;   // benchmark for decision making
 
-}
+    // team blue plays first
+    if (a > b) {    
+        teamSide = 1;
+        std::cout << "Blue team plays first." << std::endl;
+    }   // team red plays first
+    else { 
+        teamSide = -1;
+        std::cout << "Red team plays first." << std::endl;
+    }
+
+    std::cout << "The first player has 2 actions left." << std::endl;
+    actionsLeft = 2;    // set number of actions in the first turn
+ }
