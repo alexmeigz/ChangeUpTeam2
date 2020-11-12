5ff140c19105b43dd3543b9d4b9dbd4f2b5c97b6
Vedad Bassari <60596059+vedad-bassari@users.noreply.github.com>
2020-11-11 16:58:20 -0800

    Constructor Update #2
    I have applied the changes from the previous commit. The constructor does the following: 
    - Randomly choose one side to begin the game
    - Set victory condition to false at the beginning
    - Set actions Left to 2 for the starting team

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -4,19 +4,19 @@
 TicTacToe::TicTacToe()
 {
     srand(time(NULL));  // initialize random number generator
-    int a = rand();     // generate random number
-    int b = RAND_MAX / 2;   // benchmark for decision making
 
-    // team blue plays first
-    if (a > b) {    
-        teamSide = 1;
-        std::cout << "Blue team plays first." << std::endl;
-    }   // team red plays first
+    // use random condition to determine the first player
+    if (rand()>(RAND_MAX/2)) {    
+        teamSide = 1;       // team blue plays first
+        std::cout << "Blue team plays first." << std::endl; 
+    }   
     else { 
-        teamSide = -1;
+        teamSide = -1;      // team red plays first
         std::cout << "Red team plays first." << std::endl;
     }
 
     std::cout << "The first player has 2 actions left." << std::endl;
     actionsLeft = 2;    // set number of actions in the first turn
+    
+    victory = 0;    //set victory to false
  }
