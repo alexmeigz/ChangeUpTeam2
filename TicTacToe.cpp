26e006d93a1f6ef20c05675b0e33c52fe0ac4841
Vedad Bassari <60596059+vedad-bassari@users.noreply.github.com>
2020-11-11 17:30:31 -0800

    Vedad bassari (#25)
    * Header Update #1
    
    The header does the following:
    - Select a random side to begin the game.
    - Assign the number of actions left (2 in the first turn).
    
    Please let me know if the header should do anything else, or if the script is problematic. Thank you.
    
    * Constructor Update #2
    
    I have applied the changes from the previous commit. The constructor does the following: 
    - Randomly choose one side to begin the game
    - Set victory condition to false at the beginning
    - Set actions Left to 2 for the starting team

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,6 +1,22 @@
 #include "TicTacToe.h"
+#include <time.h>
 
 TicTacToe::TicTacToe()
 {
+    srand(time(NULL));  // initialize random number generator
 
-}
+    // use random condition to determine the first player
+    if (rand()>(RAND_MAX/2)) {    
+        teamSide = 1;       // team blue plays first
+        std::cout << "Blue team plays first." << std::endl; 
+    }   
+    else { 
+        teamSide = -1;      // team red plays first
+        std::cout << "Red team plays first." << std::endl;
+    }
+
+    std::cout << "The first player has 2 actions left." << std::endl;
+    actionsLeft = 2;    // set number of actions in the first turn
+    
+    victory = 0;    //set victory to false
+ }
