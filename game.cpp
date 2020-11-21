be299c565d9b58c3818375a7ddc38e2920f32c16
erinkdelong <73408261+erinkdelong@users.noreply.github.com>
2020-11-21 15:01:43 -0800

    implmented checkInput

--- game.cpp
+++ game.cpp
@@ -5,8 +5,46 @@
 TicTacToe gameBoard; //initialize game
 
 // Validates a user-input and calls the applicable TicTacToe functions.
-void checkInput(std::string input)
-{
+void checkInput(std::string userInput)
+{	using namespace std;
+	int length = userInput.size();
+    
+    for (int i = 0; i < length; i++) { //removes spaces from userInput
+        if (userInput[i] == ' ') { 
+            userInput.erase(userInput.begin() + i); 
+        } 
+    } 
+
+    string command = userInput.substr(0, 2); // get first 2 characters (should be ad or rm)
+    string coordinates = userInput.substr(userInput.length() - 2); // get last two characters
+    char xCoordinate = coordinates[0];
+    char yCoordinate = coordinates[1];
+
+    if (isdigit(xCoordinate) and isdigit(yCoordinate) and userInput.size() == 4) { //checks that the last two characters are numbers and that userInput has no other non-space characters  
+        int x = (xCoordinate - '0'); //converts char to int
+        int y = (yCoordinate - '0');
+
+        if ((2 < x) ||(x < 0) || (2 < y) || (y < 0)) { //checks that values of x and y are 0, 1, or 2, otherwise exits the function
+            cout << "Invalid Input";
+            return;
+        }
+
+        if (command == "ad") {
+            add(x, y); 
+        }
+
+        else if (command == "rm") {
+            remove(x, y); 
+        } 
+
+        else { 
+            cout << "Invalid Input" << endl;
+        } 
+    }
+
+    else {
+        cout << "Invalid Input";
+    }
 
 }
 
